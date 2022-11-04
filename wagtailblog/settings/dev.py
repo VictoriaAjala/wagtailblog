232231472ae941c6d473d962c685f5b4d4cceee5
VictoriaAjala <victoria.o.ajala@gmail.com>
2022-11-04 08:01:13 +0100

    commit entire application

---
+++ wagtailblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1(qe7*(fai%0@5f^89)2_c!_our_15-opq2l2-#vn$b&v55j5l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
